174f162b153323378d4a4a2b6410993014aca6cf
Lin040 <1978331141@qq.com>
2022-07-13 16:01:44 +0800

    登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <el-form
+        ref="loginFormRef"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+      >
+        <!-- usernanme -->
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user-solid"
+            v-model="loginForm.username"
+            placeholder="输入用户名"
+          ></el-input>
+        </el-form-item>
+        <!-- password -->
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="el-icon-s-goods"
+            v-model="loginForm.password"
+            type="password"
+            placeholder="输入密码"
+          ></el-input>
+        </el-form-item>
+        <!-- button -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    // 重置
+    resetLoginForm() {
+      // console.log(this.$refs.loginFormRef);
+      this.$refs.loginFormRef.resetFields();
+    },
+    // 登录
+    login() {
+      this.$refs.loginFormRef.validate(async (vaild) => {
+        if (!vaild) return;
+
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200)
+          return this.$message({ message: "登录失败", type: "error" });
+
+        const h = this.$createElement;
+        this.$message({
+          message: h("p", null, [
+            h("span", null, "登录成功 "),
+            h("i", { style: "color: teal" }, this.loginForm.username),
+          ]),
+          type: "success",
+        });
+        window.sessionStorage.setItem("token", res.data.token);
+
+        this.$router.push({
+          path: "/home",
+        });
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+  display: flex;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import './assets/css/global.css'
+import { Message } from 'element-ui'
+import axios from 'axios'
+
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 
